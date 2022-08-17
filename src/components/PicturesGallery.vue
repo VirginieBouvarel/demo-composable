95393215486fb213acb0b3ffdbf84f90449fba78
Virginie <vb@ikimo9.com>
2022-08-17 09:11:53 +0200

    retour version simple composants

--- src/components/PicturesGallery.vue
+++ src/components/PicturesGallery.vue
@@ -3,17 +3,12 @@
     <ul>
       <li v-for="item in currentItems" :key="item.id">
         <img :src="item.source" :alt="`Paysage de ${item.photographer}`">
-          <h4>{{ item.photographer }}</h4>
-        <div class="infos">
-          <span>{{ item.date }}</span>
-          <span>#{{ item.id }}</span>
-        </div>
+        <h4>{{ item.photographer }}</h4>
       </li>
     </ul>
     <div class="actions">
-      <button @click="sortById">#ID</button>  
-      <button @click="sortByPhotographer">A - Z</button>
-      <button @click="sortByDate">Dates</button>  
+      <button @click="sortItemsBy('photographer')">A - Z</button>
+      <button @click="sortItemsBy('id')">Initial</button>
     </div>
   </div>
 </template>
@@ -24,42 +19,39 @@
     name: 'PicturesGallery',
     setup() {
       const items = ref([
-        { id: 100, source: 'https://picsum.photos/id/100/250/150', photographer: 'Tina Rataj', date: '2019-08-15' },
-        { id: 1000, source: 'https://picsum.photos/id/1000/250/150', photographer: 'Lukas Budimaier', date: '2019-02-23' },
-        { id: 1002, source: 'https://picsum.photos/id/1002/250/150', photographer: 'NASA', date: '2012-03-12' },
-        { id: 1011, source: 'https://picsum.photos/id/1011/250/150', photographer: 'Roberto Nickson', date: '2016-05-15' },
-        { id: 1016, source: 'https://picsum.photos/id/1016/250/150', photographer: 'Philippe Wuyts', date: '2018-08-23' },
-        { id: 1018, source: 'https://picsum.photos/id/1018/250/150', photographer: 'Andrew Ridley', date: '2021-10-13' },
-        { id: 1019, source: 'https://picsum.photos/id/1019/250/150', photographer: 'Patrick Fore', date: '2021-01-23' },
-        { id: 1021, source: 'https://picsum.photos/id/1021/250/150', photographer: 'Frances Gunn', date: '2021-11-07' },
-        { id: 1022, source: 'https://picsum.photos/id/1022/250/150', photographer: 'Vashishtha Jogi', date: '2013-01-17' },
+        { id: 100, source: 'https://picsum.photos/id/100/250/150', photographer: 'Tina Rataj' },
+        { id: 1000, source: 'https://picsum.photos/id/1000/250/150', photographer: 'Lukas Budimaier' },
+        { id: 1002, source: 'https://picsum.photos/id/1002/250/150', photographer: 'NASA' },
+        { id: 1011, source: 'https://picsum.photos/id/1011/250/150', photographer: 'Roberto Nickson' },
+        { id: 1015, source: 'https://picsum.photos/id/1015/250/150', photographer: 'Alexey Topolyanskiy' },
+        { id: 1016, source: 'https://picsum.photos/id/1016/250/150', photographer: 'Philippe Wuyts' },
+        { id: 1018, source: 'https://picsum.photos/id/1018/250/150', photographer: 'Andrew Ridley' },
+        { id: 1019, source: 'https://picsum.photos/id/1019/250/150', photographer: 'Patrick Fore' },
+        { id: 1021, source: 'https://picsum.photos/id/1021/250/150', photographer: 'Frances Gunn' },
+        { id: 1022, source: 'https://picsum.photos/id/1022/250/150', photographer: 'Vashishtha Jogi' },
+        { id: 1028, source: 'https://picsum.photos/id/1028/250/150', photographer: 'Dikaseva' },
+        { id: 1032, source: 'https://picsum.photos/id/1032/250/150', photographer: 'NASA' },
+        { id: 1036, source: 'https://picsum.photos/id/1036/250/150', photographer: 'Wolfgang Lutz' },
+        { id: 1041, source: 'https://picsum.photos/id/1041/250/150', photographer: 'Tim Marshall' },
+        { id: 1043, source: 'https://picsum.photos/id/1043/250/150', photographer: 'Christian Joudrey' },
+        { id: 1051, source: 'https://picsum.photos/id/1051/250/150', photographer: 'Ales Krivec' },
       ]);
       const currentItems = ref([]);
 
-      function sortByPhotographer() {
+      function sortItemsBy(criteria) {
         currentItems.value = items.value.sort((a, b) => {
-          if (a.photographer < b.photographer) return -1;
-          if (a.photographer > b.photographer) return 1;
+          if (a[criteria] < b[criteria]) return -1;
+          if (a[criteria] > b[criteria]) return 1;
           return 0;
         });
       }
-      function sortById() {
-        currentItems.value = items.value.sort((a, b) => a.id - b.id);
-      }
-      function sortByDate() {
-        currentItems.value = items.value.sort((a, b) => {
-          return new Date(a.date) - new Date(b.date);
-        });
-      }
 
-      sortById();
+      sortItemsBy('id');
 
-      return { 
-        currentItems, 
-        sortByPhotographer, 
-        sortById, 
-        sortByDate 
-      };
+      return {
+        currentItems,
+        sortItemsBy,
+      }
     },
   }
 </script>
@@ -69,7 +61,7 @@
   border: solid 1px #c0c0c0;
   border-radius: 3px;
   padding: 10px;
-  max-width: 808px;
+  max-width: 1070px;
 }
 ul {
   list-style-type: none;
@@ -89,28 +81,13 @@ li h4 {
   margin: 5px 0;
   color: grey;
 }
-.actions {
-  display: flex;
-  justify-content: space-between;
-}
 .actions button {
   margin: 5px;
-  padding: 0;
   width: 250px;
   height: 40px;
   border: none;
   background-color: #ADD8E6;
   border-radius: 3px;
   color: white;
-}
-.infos {
-  display:flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0 10px 10px;
-}
-.infos span {
-  color: grey;
-  font-size: 0.8rem;
 }
 </style>
